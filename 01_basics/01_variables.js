a4157c81b7330d98b5e28beef3c5505ecb4f3a4d
sharib45 <sharibshahid45@gmail.com>
2024-05-03 17:51:23 +0530

    ss

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,4 +6,20 @@ var accountpass = "12345"
 
 accountCity = "chandpur"
 
+let accountstate;
 
+
+// accountId= 5 const can not be change
+
+
+accountEmail = "shhhh@gmail.com"
+accountpass = "455550"
+
+accountCity = "delhi"
+
+
+
+
+console.log(accountId)
+
+console.table([accountEmail,accountpass,accountCity,accountstate])
