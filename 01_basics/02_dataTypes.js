743efafc875ceb52c2c4f1a91ce3d394a2871fd4
sharib45 <sharibshahid45@gmail.com>
2024-05-29 22:41:39 +0530

    ff

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,29 @@
-s
+"use strict";
+
+console.log(3+3) 
+
+console.log("sharib")
+
+// primative data type
+
+let age = 18;   // number
+
+let bigNum = 123422222222222222222222222222222222222n // bigint
+
+let name ="sharib" // string
+
+
+let is = true  // boolean 
+
+
+let weather = null // null
+
+let city //undefined
+
+
+const sym1 = Symbol(); // symbol 
+
+// non-primative
+
+
+//object
