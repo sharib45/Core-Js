42114347577af4ec3f77dc34693a6de2ccece915
sharib45 <sharibshahid45@gmail.com>
2024-05-30 21:08:52 +0530

    sss

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,57 @@
-let score = '33abc'
+
+//let score = '33'
+//let score = '33abc'
+
+//let score = null;
+
+//let score = undefined
+
+//let score = "sharib"
 
 
-console.log(typeof score)
+//let score = true;
+
+/*console.log(typeof score)
 
 let valuveinNumber = Number(score)
 
 
 console.log(typeof valuveinNumber)
 
-console.log(valuveinNumber)
+console.log(valuveinNumber) 
+*/
+
+
+//let is_login = 1 
+
+//let is_login ="sharib"
+
+//let is_login =""
+
+//let is_login = null
+
+//let is_login = 1234
+
+//let is_login = undefined
+
+//let boleanLog_in = Boolean(is_login)
+
+
+/*console.log(typeof is_login)
+
+console.log(typeof boleanLog_in)
+
+console.log(boleanLog_in)
+
+*/
+
+let convertString = 333
+
+let isString = String(convertString)
+
+
+console.log(typeof convertString)
+
+console.log(typeof isString)
+
+console.log(isString)
