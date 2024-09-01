2f1975610722999384be99176ac245569e6277eb
sharib45 <sharibshahid45@gmail.com>
2024-09-01 23:14:19 +0530

    876867

---
+++ 01_basics/06_string.js
@@ -0,0 +1,68 @@
+//num 
+
+
+/*const score = 400
+
+const balance = new Number(100)
+
+console.log(score)
+
+console.log(balance)
+
+
+console.log(balance.toString());
+
+console.log(balance.toFixed(3));
+
+const othernumber = 23.46255566
+
+const newnum =othernumber.toPrecision(4);
+
+console.log(newnum)
+
+console.log(typeof newnum);
+
+
+
+const hundred = 1000000
+
+
+console.log(hundred.toLocaleString('en-IN'));
+
+
+*/
+
+
+//+++++ MATH
+
+
+
+/*console.log(Math)
+
+
+console.log(Math.abs(-5))
+
+console.log(Math.round(9.5))
+
+console.log(Math.ceil(9.2))
+
+console.log(Math.floor(9.2))
+
+console.log(Math.min(5,8,12))
+
+console.log(Math.max(5,8,12))
+
+*/
+
+console.log(Math.random())
+console.log((Math.random()*10)+1);
+console.log((Math.floor(Math.random()*10)+1))
+
+const min = 10;
+
+const max = 20;
+
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
